826591efffed08bc6a50e0d73aaf1d03a9ad5428
Steven Chen <schenn1992@gmail.com>
2021-04-10 00:06:26 -0400

    create route for get a single todo

--- server/index.js
+++ server/index.js
@@ -40,6 +40,15 @@ app.get("/todos", async(req, res) => {
 
 
 //get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE id = $1", [id])
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+})
 
 
 //update a todo
