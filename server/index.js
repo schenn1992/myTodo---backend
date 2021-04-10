142a13d785c84cdb647fdf47fbaf6283cd1df722
Steven Chen <schenn1992@gmail.com>
2021-04-09 23:24:13 -0400

    create route for creating a todo

--- server/index.js
+++ server/index.js
@@ -1,8 +1,45 @@
 const express = require("express");
 const app = express();
-
+const cors = require("cors");
 const port = 5000;
+const pool = require("./db");
 
-app.listen(port,() => {
+//middleware
+app.use(cors());
+
+//enable to access body data
+app.use(express.json())
+
+//ROUTES//
+//create a todo
+app.post("/todos", async(req, res) => {
+    try {
+      const { description } = req.body;
+      const newTodo = await pool.query(
+      'INSERT INTO todo (description) VALUES ($1)', 
+      [description]
+    )
+      res.json(newTodo)
+    } catch (err) {
+      console.error(err.message);
+    }
+    
+    
+});
+
+
+//display all todos
+
+
+//get a todo
+
+
+//update a todo
+
+
+//delete a todo
+
+
+app.listen(port, () => {
   console.log(`myTodo app is listening at http://localhost:${port}`)
 })
